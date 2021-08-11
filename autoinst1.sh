355df403fb9724c22b47067e3d3244e4bd28e4a2
KHoudayer <87358347+KHoudayer@users.noreply.github.com>
2021-08-11 16:16:33 +0200

    Update autoinst1.sh

--- autoinst1.sh
+++ autoinst1.sh
@@ -26,10 +26,29 @@ else
         echo -e  "pas d'ip selectionner"
         exit 3;
 fi
+echo -n "Entrer le subnet du serveur voulu (ex 24)':   "
+read subnet
+if [[ -n "$subnet" ]]
+then
+        echo -e "le subnet : $subnet est saisi"
+else
+        echo -e  "pas de subnet selectionner"
+        exit 3;
+fi
 
+echo -n "Entrer la gateway du serveur voulu':   "
+read gateway
+if [[ -n "$gateway" ]]
+then
+        echo -e "la gateway : $gateway est saisi"
+else
+        echo -e  "pas de gateway selectionner"
+        exit 3;
+fi
 
-sed -i "s/172.31.101.106/$ipadd/" /etc/sysconfig/network-scripts/ifcfg-ens192
-
+sed -i "s/172.31.101.134/$ipadd/" /etc/sysconfig/network-scripts/ifcfg-ens192
+sed -i "s/172.31.101.254/$gateway/" /etc/sysconfig/network-scripts/ifcfg-ens192
+sed -i "s/PREFIX=23/PREFIX=$subnet/" /etc/sysconfig/network-scripts/ifcfg-ens192
 
 echo -n "le serveur va rebooter dans 10 sec faites ctrl + c pour annuler"
 sleep 10
